c49367b69c85518ba0427b6e0af85907e48371ba
hugo <hhldiniz@gmail.com>
2018-06-08 17:19:46 -0300

    funço de validacao da blockchain

--- __init__.py
+++ __init__.py
@@ -1,11 +1,16 @@
 # -*-encoding:utf-8-*-
 
+import json
+
 from Block import Block
 
 
-def is_valid_block(block):
-    for b in blocks:
-        pass
+def is_chain_valid():
+    for i, b in enumerate(blocks):
+        if b.get_hash() != 0:
+            if b.get_previous_hash() != blocks[i-1].get_hash():
+                return False
+    return True
 
 
 if __name__ == '__main__':
@@ -15,7 +20,6 @@ if __name__ == '__main__':
     total_c2 = 0
     total_c3 = 0
     while True:
-        name = input("Digite seu nome\n")
         print("1 - Votar")
         print("2 - Contabilizar")
         print("3 - Fechar urna")
@@ -34,12 +38,27 @@ if __name__ == '__main__':
             else:
                 total_c3 += 1
             print("Voto salvo com sucesso!")
-        elif op == "3":
-            new_block = Block(blocks.__getitem__(blocks.__len__()-1))
-            new_block.set_data({"c1": total_c1, "c2": total_c2, "c3": total_c3})
         elif op == "2":
-            for index, block in enumerate(blocks):
-                if block.get_previous_hash() != 0:
-                    pass
+            if blocks.__len__() == 1:
+                genesis.set_data(json.dumps({"c1": total_c1, "c2": total_c2, "c3": total_c3}))
+            new_block = Block(blocks.__getitem__(blocks.__len__() - 1))
+            new_block.set_data(json.dumps({"c1": total_c1, "c2": total_c2, "c3": total_c3}))
+            if is_chain_valid():
+                blocks.append(new_block)
+        elif op == "3":
+            if is_chain_valid():
+                t1 = 0
+                t2 = 0
+                t3 = 0
+                for block in blocks:
+                    data = json.loads(block.get_data())
+                    t1 += data["c1"]
+                    t2 += data["c2"]
+                    t3 += data["c3"]
+                print(f"Total do candidato 1: {t1}")
+                print(f"Total do candidato 2: {t2}")
+                print(f"Total do candidato 3: {t3}")
+            else:
+                print("Blockchain e invalida!")
         elif op == "0":
             exit(0)
